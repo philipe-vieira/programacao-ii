f86c02446c220f4feba27656abda7fca5d866819
Ada <adabeatriz.oa@gmail.com>
2021-08-24 23:58:21 -0300

    get e set
    atributos private + get e set

--- Projeto/TrabalhoFinal/src/main/java/TrabalhoFinal/JavaBeanAluno.java
+++ Projeto/TrabalhoFinal/src/main/java/TrabalhoFinal/JavaBeanAluno.java
@@ -10,10 +10,60 @@ package TrabalhoFinal;
  * @author ada
  */
 public class JavaBeanAluno {
-    int matricula;
-    String nome;
-    int idade;
-    String dataNascimento;
-    String telefone;
-    String cpf;
+   private int matricula;
+   private String nome;
+   private int idade;
+   private String dataNascimento;
+   private String telefone;
+   private String cpf;
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+   
+   
 }
